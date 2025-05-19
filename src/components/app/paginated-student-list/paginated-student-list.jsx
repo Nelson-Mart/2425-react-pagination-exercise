5ec54fabb87de71fcf14141dfcc59153c08a1cbe
Nelson-Mart <nelsonmartinel2@gmail.com>
2025-05-19 13:36:18 +0200

    git init

--- src/components/app/paginated-student-list/paginated-student-list.jsx
+++ src/components/app/paginated-student-list/paginated-student-list.jsx
@@ -1,23 +1,38 @@
 import { useEffect, useState } from "react";
 import { StudentList } from "./student-list/student-list";
 import { Pagination } from "./pagination/pagination";
+import { API_TOKEN, API_URL } from "../../../constants/constants";
 
 export function PaginatedStudentList() {
   const [currentPage, setCurrentPage] = useState(1);
   const [students, setStudents] = useState([]);
 
-  const pageCount = 86;
+  const pageCount = students.length;
 
   function handlePageChanged(pageNumber) {
     setCurrentPage(pageNumber);
   }
 
-  // Up to you to use useEffect properly here
+  useEffect(() => {
+  fetch(`${API_URL}`, {
+    headers: {
+      "Authorization": `Bearer ${API_TOKEN}`
+    }
+  })
+    .then((response) => response.json())
+    .then((jsonData) => {
+      console.log(jsonData);
+      setStudents(jsonData.data);
+    })
+    .catch((error) => {
+      console.error("Error fetching students:", error);
+    });
+}, []); 
 
   return (
     <>
       <div style={{ marginBottom: "2rem" }}>
-        <StudentList students={students} />
+        <StudentList students={students[currentPage]} />
       </div>
       <Pagination
         currentPage={currentPage}
